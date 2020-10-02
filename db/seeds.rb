175f7e8d67b1059c5a2ff699c74aa4e06a0bd0d9
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-03 00:48:24 +0300

    Update seed file with invitations

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,27 @@ User.create(name:"Victoria")
 puts "Users Created!"
 
 
-5.times do |e|
+4.times do |e|
  event = Event.new
  event.name = "The #{e}'st party"
  event.creator = User.first
  event.location = "Sf. Andrei 4#{e}"
  event.datetime = Time.now + 10.day
+ event.description = "#{e}. Aperiam itaque, eligendi, aspernatur, vel asperiores necessitatibus tempore omnis distinctio libero quae labor"
  event.save
 end
 
-puts "Events created"
+puts "Events created successfully!"
+
+
+3.times do |i|
+invite = Invitation.new
+invite.event_party = Event.first
+invite.person_to_come = User.first
+invite.person_to_come = User.second
+invite.person_to_come = User.third
+invite.save
+end
+
+
+puts "Invitations created successfully!"
