c655770e1b11f65e0e60b7f9432066a08410a76d
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-02 18:57:31 +0300

    add validation to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
   belongs_to :user
-
 
   has_many :invitations,  foreign_key: :event_party
   has_many :person_to_comes, through: :invitations
