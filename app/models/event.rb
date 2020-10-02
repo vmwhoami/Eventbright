3fc88f2efc866b69506246beb81d37560f09d53b
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-02 18:39:23 +0300

    Add basic associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+
+  has_many :invitations,  foreign_key: :event_party
+  has_many :person_to_comes, through: :invitations
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :events 
 
-
-  
+ has_many :invitations, foreign_key: :person_to_come
+ has_many :event_parties, through: :invitations
 end
