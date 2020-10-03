4275b252561fc92d4862a65e3a66b9c240aaa67a
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-03 14:32:19 +0300

    Add basic ui functionality to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(permited_params)
+   
+    begin
+      @user.save 
+      redirect_to user_path
+    rescue => exception
+      flash.now[:error] = "Sorry"
+      render "new"
+    else
+      redirect_to root_path
+    end
   end
 
   def show
