53e6e369063f791aaf38f24faf56c5ac2bb6d5bc
vmwhoami <vmwhoami@gmail.com>
2021-03-18 16:34:37 +0200

    Run linters fix all errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,36 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_006_204_308) do
+ActiveRecord::Schema.define(version: 2020_10_06_204308) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'events', force: :cascade do |t|
-    t.bigint 'creator_id', null: false
-    t.string 'name'
-    t.string 'location'
-    t.date 'datetime'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'description'
-    t.index ['creator_id'], name: 'index_events_on_creator_id'
+  create_table "events", force: :cascade do |t|
+    t.bigint "creator_id", null: false
+    t.string "name"
+    t.string "location"
+    t.date "datetime"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
-  create_table 'invitations', force: :cascade do |t|
-    t.integer 'event_party_id'
-    t.integer 'person_to_come_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[event_party_id person_to_come_id], name: 'index_invitations_on_event_party_id_and_person_to_come_id', unique: true
-    t.index ['event_party_id'], name: 'index_invitations_on_event_party_id'
-    t.index ['person_to_come_id'], name: 'index_invitations_on_person_to_come_id'
+  create_table "invitations", force: :cascade do |t|
+    t.integer "event_party_id"
+    t.integer "person_to_come_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_party_id", "person_to_come_id"], name: "index_invitations_on_event_party_id_and_person_to_come_id", unique: true
+    t.index ["event_party_id"], name: "index_invitations_on_event_party_id"
+    t.index ["person_to_come_id"], name: "index_invitations_on_person_to_come_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'events', 'users', column: 'creator_id'
+  add_foreign_key "events", "users", column: "creator_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,55 +2,55 @@
 # # This file should contain all the record creation needed to seed the database with its default values.
 # # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # #
-# Event.destroy_all
-# User.destroy_all
-# Invitation.destroy_all
+Event.destroy_all
+User.destroy_all
+Invitation.destroy_all
 
-# User.create(name:"Vitalie")
-# User.create(name:"Olea")
-# User.create(name:"Adama")
-# User.create(name:"Sergiu")
-# User.create(name:"Ina")
-# User.create(name:"Galina")
-#   20.times do |user|
-#     User.create(name:"user_#{user}")
-#   end
+User.create(name:"Vitalie")
+User.create(name:"Olea")
+User.create(name:"Adama")
+User.create(name:"Sergiu")
+User.create(name:"Ina")
+User.create(name:"Galina")
+  20.times do |user|
+    User.create(name:"user_#{user}")
+  end
 
-# puts "--"*100
-# puts "Users Created!"
-# b = 1
-# 5.times do |e|
-#   b+=1
-#   Event.create(
-#     name:"Event #{e}",
-#     creator:User.find_by_id(b),
-#     location: "Alecu Russo #{e}",
-#     datetime:Time.now + e.day
+puts "--"*100
+puts "Users Created!"
+b = 1
+5.times do |e|
+  b+=1
+  Event.create(
+    name:"Event #{e}",
+    creator:User.find_by_id(b),
+    location: "Alecu Russo #{e}",
+    datetime:Time.now + e.day
+  )
+ end
+
+ 5.times do |e|
+  b+=1
+  Event.create(
+    name:"A fost #{e}",
+    creator:User.find_by_id(b),
+    location: "Sfintul Andrei #{e}",
+    datetime:Time.now - e.day
+  )
+ end
+
+ puts "--"*100
+puts "Events created successfully!"
+
+# 7.times do |i|
+#   Invitation.create(
+#     event_party:Event.first,
+#     person_to_come:User.find(b)
 #   )
-#  end
-
-#  5.times do |e|
 #   b+=1
-#   Event.create(
-#     name:"A fost #{e}",
-#     creator:User.find_by_id(b),
-#     location: "Sfintul Andrei #{e}",
-#     datetime:Time.now - e.day
-#   )
-#  end
-
-#  puts "--"*100
-# puts "Events created successfully!"
-
-# # 7.times do |i|
-# #   Invitation.create(
-# #     event_party:Event.first,
-# #     person_to_come:User.find(b)
-# #   )
-# #   b+=1
-# # end
-# # Invitation.create(event_party: Event.third,person_to_come:User.second)
-# # Invitation.create(event_party: Event.third,person_to_come:User.third)
-# # Invitation.create(event_party: Event.third,person_to_come:User.fourth)
-# puts "--"*100
-# puts "Invitations created successfully!"
+# end
+# Invitation.create(event_party: Event.third,person_to_come:User.second)
+# Invitation.create(event_party: Event.third,person_to_come:User.third)
+# Invitation.create(event_party: Event.third,person_to_come:User.fourth)
+puts "--"*100
+puts "Invitations created successfully!"
